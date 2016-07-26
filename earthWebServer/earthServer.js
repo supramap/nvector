65ddb311cdaa81928a81fa3a8ca21bddb77c84c8
Zach <zwitter1@uncc.edu>
2016-07-26 11:37:22 -0400

    the earth webserver now also hosts a restful service

--- earthWebServer/earthServer.js
+++ earthWebServer/earthServer.js
@@ -1,7 +1,10 @@
 var static = require('node-static');
 var os = require('os');
+var dispatcher = require("httpdispatcher");
+var mongoc = require('mongodb').MongoClient;
+var url = require('url')
 
-var fileServer = new static.Server('../earth/NVector');
+var fileServer = new static.Server('../earth');
 var ifaces = os.networkInterfaces();
 
 console.log("Server running");
@@ -28,8 +31,51 @@ Object.keys(ifaces).forEach(function (ifname) {
 });
 
 require('http').createServer(function (request, response) {
-	console.log("HIT")
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
-    }).resume();
+  dispatcher.dispatch(request, response);
+
 }).listen(8080);
+
+
+
+
+dispatcher.onGet("/", function(req,res){
+
+  req.addListener('end', function () {
+      fileServer.serve(req, res);
+  }).resume();
+});
+
+
+dispatcher.onGet("/showFDA",function(req,res){
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  mongoc.connect("mongodb://192.168.1.13:27017/nvector", function(err,db){
+    if(err){
+      console.log("an error was reported: " + err );
+      res.end("An error was returned. View developer console");
+      return;
+    }
+    var col = db.collection('fda');
+    var arr = col.find({},{"metadata":1}).toArray(function(err, results){
+        res.end(JSON.stringify(results));
+    });
+
+  });
+});
+
+dispatcher.onGet("/getFDA", function(req,res){
+  res.writeHead(200, {'Content-Type': 'application/json'});
+
+  var fileName = req.params["fileName"];
+  mongoc.connect("mongodb://192.168.1.13:27017/nvector", function(err,db){
+    if(err){
+      console.log("an error was reported " + err);
+      res.end("and error was returned. View developer console");
+    }
+    var col = db.collection('fda');
+    col.findOne({"metadata.fileName":fileName},function(err,results){
+      res.end(JSON.stringify(results));
+    });
+  });
+
+
+});
